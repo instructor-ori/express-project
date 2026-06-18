24b3db2026b3fed4e230648235d6b6688bbdd7d5
Ori Avraham <mr.ori.avraham@gmail.com>
2026-06-19 01:00:35 +0700

    test: add mock crud REST API endpoint

--- src/app.js
+++ src/app.js
@@ -2,14 +2,31 @@ import express from "express";
 
 const app = express();
 
-// GET / (http://localhost:3000)
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+// GET /users (all users)
+app.get("/users", (req, res) => {
+  res.send("All users");
 });
 
-// GET /message (http://localhost:3000/message)
-app.get("/message", (req, res) => {
-  res.send({ message: "This is the daily message!" });
+// GET /users/:id (user by id)
+app.get("/users/:id", (req, res) => {
+  const id = req.params.id;
+  res.send(`User #${id}`);
+});
+
+// POST /users (create new user)
+app.post("/users", (req, res) => {
+  res.send("User created!");
+});
+
+// PATCH /users/:id (update user by id)
+app.patch("/users/:id", (req, res) => {
+  const id = req.params.id;
+  res.send(`User #${id} updated!`);
+});
+
+app.delete("/users/:id", (req, res) => {
+  const id = req.params.id;
+  res.send(`User #${id} deleted!`);
 });
 
 export default app;
