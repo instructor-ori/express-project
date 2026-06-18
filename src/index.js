659a29834887dd500114df8c8b752b8715fab430
Ori Avraham <mr.ori.avraham@gmail.com>
2026-06-19 00:24:03 +0700

    feat: add new endpoint '/message'

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
-import express from 'express';
+import express from "express";
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// GET / (http://localhost:3000)
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+// GET /message (http://localhost:3000/message)
+app.get("/message", (req, res) => {
+  res.send({ message: "This is the daily message!" });
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
